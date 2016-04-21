a2b3ffeb95c725b47db83b744e02b5a4db27fbd8
alathans <alathans@users.noreply.github.com>
2016-04-21 15:42:30 -0500

    done with wordcount part

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -14,9 +14,9 @@ class CalculationsController < ApplicationController
 
     @character_count_without_spaces = @text.gsub(" ","").length
 
-    @word_count = "Replace this string with your answer."
+    @word_count = @text.split.size
 
-    @occurrences = "Replace this string with your answer."
+    @occurrences = @text.scan(@special_word)
 
     # ================================================================================
     # Your code goes above.
