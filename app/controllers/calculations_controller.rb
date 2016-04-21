2810b3009b514d372338e9c50c9097258ab8e815
alathans <alathans@users.noreply.github.com>
2016-04-21 13:03:24 -0500

    beginning

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -10,10 +10,9 @@ class CalculationsController < ApplicationController
     # The special word the user input is in the string @special_word.
     # ================================================================================
 
+    @character_count_with_spaces = @text.length
 
-    @character_count_with_spaces = "Replace this string with your answer."
-
-    @character_count_without_spaces = "Replace this string with your answer."
+    @character_count_without_spaces = @text.gsub(" ","").length
 
     @word_count = "Replace this string with your answer."
 
