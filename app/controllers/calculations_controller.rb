03181702dcf9f9293f13479dd0a66491b3918a2f
meghankinney <meghankinney@users.noreply.github.com>
2016-04-21 15:43:35 +0000

    Thursday test

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -9,7 +9,7 @@ class CalculationsController < ApplicationController
     # The text the user input is in the string @text.
     # The special word the user input is in the string @special_word.
     # ================================================================================
-
+# another small change
 
     @character_count_with_spaces = @text.length
 
