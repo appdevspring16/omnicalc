258d79e86f26869bce2152144f7ddf5220ae7f03
meghankinney <meghankinney@users.noreply.github.com>
2016-04-22 00:44:54 +0000

    word calculator

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -13,11 +13,11 @@ class CalculationsController < ApplicationController
 
     @character_count_with_spaces = @text.length
 
-    @character_count_without_spaces = "Replace this string with your answer."
+    @character_count_without_spaces = @text.gsub(" ", "").length
 
-    @word_count = "Replace this string with your answer."
+    @word_count = @text.titleize.scan(/[A-Z]/).length
 
-    @occurrences = "Replace this string with your answer."
+    @occurrences = @text.titleize.scan(/\w+/).count(@special_word.capitalize)
 
     # ================================================================================
     # Your code goes above.
