6aeae7314209912696d97dee5ebbb68b023c206f
Cristina Costa <cncosta25@gmail.com>
2016-04-22 10:48:15 -0500

    median mode
    figured it out, but want to see how mode can display without the []

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -99,19 +99,70 @@ class CalculationsController < ApplicationController
 
     @range = @maximum - @minimum
 
-    @median = "Replace this string with your answer."
+    #median
+
+    odd_middle_position = (@count-1)/2
+    even_first_middle= (@count-1)/2
+    even_second_middle= ((@count-1)/2) + 1
+
+    if @count.odd? == true
+      median=@sorted_numbers[odd_middle_position.to_i]
+    elsif @count.odd? == false
+      median= ((@sorted_numbers[even_first_middle] + @sorted_numbers[even_second_middle])/2).to_f
+    end
+
+    @median=median
 
     @sum = @numbers.sum
 
     @mean = @sum / @count
 
+    def variance(list_of_numbers)
+      number_mean_diff = 0
+      list_of_numbers.each do |number|
+        number_mean_diff = number_mean_diff + ((number - @mean)**2)
+      end
+      return number_mean_diff/@count
+    end
 
-
-    @variance = 1
+    @variance = variance(@numbers)
 
     @standard_deviation = @variance**0.5
 
-    @mode = "Replace this string with your answer."
+    # I found a helpful github about calculating mode and I almost understand it all given his/her nice comments
+    # but I stll want to review some parts below (namely the last if statement)
+
+    def mode(list_of_numbers)
+      frequency = Hash.new(0)
+      # -- other person's  note --
+      # this creates a new, empty hash with no keys, but makes all defalt values zero. it will be used to store
+      # the information from the array, such that the keys will be each unique number from the array (IOW, if there
+      # are two or more 4's in the array, there will just be one key that is a 4), and the value for each key will
+      # be the number of times that number appears in the array.
+      list_of_numbers.each do |number|
+        frequency[number] += 1
+      end
+      # -- other person's note --
+      # this interates throught the array, and for each element it creates a key for that number (if it hasn't been
+      # created already) and basically pushes a +1 to the value of that key. remember, the values all start at zero
+      # because the "Hash.new" language included "(0)" at the end.
+      mode_array = []
+      # --other person's note --
+      # this creates an empty array, to be filled with the most repeated number(s) from the array.
+      # this will be the array that is returned at the end of the method.
+      frequency.each do |k, v|
+        if v == frequency.values.max
+          mode_array << k
+        end
+      end
+      # --other peron's note --
+      # this loop iterates through the frequency array. for each key/value pair, it compares the value to
+      # the high value in the array, and if the value it's comparing is equal to the high value, it
+      # adds to the mode array the key associated with that value.
+      mode_array.sort
+    end
+
+    @mode = mode(@numbers)
 
     # ================================================================================
     # Your code goes above.
