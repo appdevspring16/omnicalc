f737c2802e08951a79c2c422ecf3cc77d5b929de
Ian Williams <iwilli1@users.noreply.github.com>
2016-04-23 12:38:07 -0500

    Final

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -16,7 +16,7 @@ class CalculationsController < ApplicationController
     @character_count_without_spaces = (@text.gsub(" ","")).length
 
     @word_count = @character_count_with_spaces - @character_count_without_spaces + 1
-    @occurrences =
+    @occurrences = @text.split(" ").count(@special_word)
 
     # ================================================================================
     # Your code goes above.
@@ -81,27 +81,28 @@ class CalculationsController < ApplicationController
     # The numbers the user input are in the array @numbers.
     # ================================================================================
 
-    @sorted_numbers = "Replace this string with your answer."
+    @sorted_numbers = @numbers.sort
 
-    @count = "Replace this string with your answer."
+    @count = @numbers.count
 
-    @minimum = "Replace this string with your answer."
+    @minimum = @sorted_numbers[0]
 
-    @maximum = "Replace this string with your answer."
+    @maximum = @sorted_numbers.last
 
-    @range = "Replace this string with your answer."
+    @range = @maximum - @minimum
 
-    @median = "Replace this string with your answer."
+    @median = ((@sorted_numbers[(@count - 1) / 2] + @sorted_numbers[@count / 2]) / 2.0).to_f
 
-    @sum = "Replace this string with your answer."
+    @sum = @numbers.sum
 
-    @mean = "Replace this string with your answer."
+    @mean = @sum/@count
 
-    @variance = "Replace this string with your answer."
+    @variance = "Um . . . "
 
-    @standard_deviation = "Replace this string with your answer."
+    @standard_deviation = "Err . . . . "
 
-    @mode = "Replace this string with your answer."
+    @mode = ". . . I have to go now."
+
 
     # ================================================================================
     # Your code goes above.
