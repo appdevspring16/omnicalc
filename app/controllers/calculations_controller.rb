0f053da5bc2a6c7ee9c6fae13e0cf52ddf7c150b
Rosa Rodriguez <rrodrig1@users.noreply.github.com>
2016-04-22 22:32:30 -0500

    Adding functions

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -11,14 +11,21 @@ class CalculationsController < ApplicationController
     # ================================================================================
 
 
-    @character_count_with_spaces = "Replace this string with your answer."
+    @character_count_with_spaces = @text.length
 
-    @character_count_without_spaces = "Replace this string with your answer."
+    @character_count_without_spaces =   @text.squish.length - @text.squish.count(" ")
 
-    @word_count = "Replace this string with your answer."
+    @word_count = @text.scan(/\b/).size/2
 
-    @occurrences = "Replace this string with your answer."
+    def count_em(string, substring)
+      string.scan(/(?=#{substring})/).count
+    end
 
+    if @special_word.empty?
+      @occurrences="0"
+    else
+    @occurrences = count_em(@text, @special_word)
+    end
     # ================================================================================
     # Your code goes above.
     # ================================================================================
@@ -31,14 +38,15 @@ class CalculationsController < ApplicationController
     @years = params[:number_of_years].to_i
     @principal = params[:principal_value].to_f
 
-    # ================================================================================
-    # Your code goes below.
-    # The annual percentage rate the user input is in the decimal @apr.
-    # The number of years the user input is in the integer @years.
-    # The principal value the user input is in the decimal @principal.
+    apr = @apr
+    apr = apr.to_f/1200
+    years = @years
+    principal = @principal
+    nmonths = years.to_i*12
+    mpayment = (apr*principal.to_f)/(1-((1+apr)**(-1*nmonths.to_i)))
     # ================================================================================
 
-    @monthly_payment = "Replace this string with your answer."
+    @monthly_payment = mpayment.to_s
 
     # ================================================================================
     # Your code goes above.
@@ -52,20 +60,19 @@ class CalculationsController < ApplicationController
     @ending = Chronic.parse(params[:ending_time])
 
     # ================================================================================
-    # Your code goes below.
-    # The start time is in the Time @starting.
-    # The end time is in the Time @ending.
-    # Note: Ruby stores Times in terms of seconds since Jan 1, 1970.
-    #   So if you subtract one time from another, you will get an integer
-    #   number of seconds as a result.
-    # ================================================================================
+    tdsec = @ending - @starting
+    tdmin = tdsec.to_f/60
+    tdhour= tdsec.to_f/3600
+    tdday = tdsec.to_f/(3600*24)
+    tdweek= tdsec.to_f/(3600*24*7)
+    tdyear= tdsec.to_f/(3600*24*365)
 
-    @seconds = "Replace this string with your answer."
-    @minutes = "Replace this string with your answer."
-    @hours = "Replace this string with your answer."
-    @days = "Replace this string with your answer."
-    @weeks = "Replace this string with your answer."
-    @years = "Replace this string with your answer."
+    @seconds = tdsec.to_s
+    @minutes = tdmin.to_s
+    @hours = tdhour.to_s
+    @days = tdday.to_s
+    @weeks = tdweek.to_s
+    @years = tdyear.to_s
 
     # ================================================================================
     # Your code goes above.
@@ -77,30 +84,41 @@ class CalculationsController < ApplicationController
   def descriptive_statistics
     @numbers = params[:list_of_numbers].gsub(',', '').split.map(&:to_f)
 
-    # ================================================================================
-    # Your code goes below.
-    # The numbers the user input are in the array @numbers.
-    # ================================================================================
+    fl_array = []
+    fl_array = @numbers.map(&:to_f)
+    total = 0
+    medpos = 0
+    stdev = 0.0
+    vari = 0.0
+    vars = 0.0
+    mn = 0.0
+    fl_array.each { |num| total = total + num }
+    medpos = fl_array.length/2
+    mn = total/fl_array.length
+    fl_array.each { |num| vars = (num-mn)**2 }
+    vari = vars/total
+    stdev = vari**0.5
 
-    @sorted_numbers = "Replace this string with your answer."
 
-    @count = "Replace this string with your answer."
+    @sorted_numbers = @numbers.sort
 
-    @minimum = "Replace this string with your answer."
+    @count = fl_array.length.to_s
 
-    @maximum = "Replace this string with your answer."
+    @minimum = fl_array.sort.first.to_s
 
-    @range = "Replace this string with your answer."
+    @maximum = fl_array.sort.last.to_s
 
-    @median = "Replace this string with your answer."
+    @range = (fl_array.sort.last-fl_array.sort.first).to_s
 
-    @sum = "Replace this string with your answer."
+    @median = fl_array[medpos].to_s
 
-    @mean = "Replace this string with your answer."
+    @sum = total.to_s
 
-    @variance = "Replace this string with your answer."
+    @mean = mn.to_s
 
-    @standard_deviation = "Replace this string with your answer."
+    @variance = vari.to_s
+
+    @standard_deviation = stdev.to_s
 
     @mode = "Replace this string with your answer."
 
